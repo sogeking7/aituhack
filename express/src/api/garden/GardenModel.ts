d3e74329cc976d05662285c4e390b1e45029a4ce
Kaiyrkhan <kairolla222@gmail.com>
2025-03-30 07:38:23 +0500

    fix fix

--- express/src/api/garden/GardenModel.ts
+++ express/src/api/garden/GardenModel.ts
@@ -6,8 +6,10 @@ extendZodWithOpenApi(z)
 export const GardenCreateSchema = z.object({
   name: z.string(),
   seeding_date: z.string().optional(),
-  microgreen: z.number().optional(),
+  microgreen: z.string().optional(),
   user: z.number().optional(),
+  commentary: z.string().optional(),
+  substrate_type: z.string().optional(),
 })
 
 export const GardenSchema = z.object({
@@ -16,6 +18,17 @@ export const GardenSchema = z.object({
   seeding_date: z.string().nullable().optional(),
   microgreen: z.any().nullable().optional(),
   user: z.any().nullable().optional(),
+  commentary: z.string().optional(),
+  estimated_remaining_days: z.number().nullable().optional(),
+  journals: z.array(z.any()).nullable().optional(),
+  notification: z
+    .object({
+      enabled: z.boolean().nullable().optional(),
+      time: z.string().nullable().optional(),
+      frequency: z.string().nullable().optional(),
+    })
+    .nullable()
+    .optional(),
   createdAt: z.string(),
   updatedAt: z.string(),
 })
